27096cfeb12cc48620077fe5011578000db70c73
pashakhatamihsb <pashakhatami08@gmail.com>
2025-04-09 10:19:35 +0700

    first commit

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /// primary = #B04295
+  static const Color primary = Color(0xB04295);
+
+  /// grey = #6C757D
+  static const Color grey = Color(0xff6C757D);
+
+  /// light = #E4E7EE
+  static const Color light = Color(0xffE4E7EE);
+
+  /// black = #000000
+  static const Color black = Color(0xff000000);
+
+  /// white = #FFFFFF
+  static const Color white = Color(0xffFFFFFF);
+
+  /// red = #D34459
+  static const Color red = Color(0xffD34459);
+
+  /// stroke = #E5E8EE
+  static const Color stroke = Color(0xffE5E8EE);
+
+  /// bgLogin = #EDEDED
+  static const Color bgLogin = Color(0xffEDEDED);
+}
